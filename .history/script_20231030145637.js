7ad5fb63e48772781136ebd649c4da0d59172050
qtqqrdy13 <13qtqqrdy@gmail.com>
2023-11-04 15:36:50 +0200

    commit

---
+++ .history/script_20231030145637.js
@@ -0,0 +1,18 @@
+const exchangeRates = {
+    USD: 1,
+    UAH: 36.42,
+    EUR: 0.94,
+    ZL: 4.21,
+    GBP: 0.82, 
+}
+function convertCurrency(){
+    const amount = parseFloat(document.getElementById("amount").value);
+    const fromCurrency = document.getElementById("from_currency").value;
+    const toCurrency = document.getElementById("to_currency").value;
+    const  result = (amount / exchangeRates[fromCurrency]) * exchangeRates[toCurrency];
+    document.getElementById("result").textContent = `Result: ${result.toFixed(2)} ${toCurrency}`
+}
+document.getElementById("amount").addEventListener("input", convertCurrency);
+document.getElementById("fromCurrency").addEventListener("change",convertCurrency);
+document.getElementById("toCurrency").addEventListener("change",convertCurrency);
+convertCurrency();
---
+++ script.js
@@ -0,0 +1,18 @@
+const exchangeRates = {
+    USD: 1,
+    UAH: 36.42,
+    EUR: 0.94,
+    ZL: 4.21,
+    GBP: 0.82, 
+}
+function convertCurrency(){
+    const amount = parseFloat(document.getElementById("amount").value);
+    const from_currency = document.getElementById("from_currency").value;
+    const to_currency = document.getElementById("to_currency").value;
+    const  result = (amount / exchangeRates[from_currency]) * exchangeRates[to_currency];
+    document.getElementById("result").textContent = `Result: ${result.toFixed(2)} ${to_currency}`;
+}
+document.getElementById("amount").addEventListener("input", convertCurrency);
+document.getElementById("from_currency").addEventListener("change",convertCurrency);
+document.getElementById("to_currency").addEventListener("change",convertCurrency);
+convertCurrency();
